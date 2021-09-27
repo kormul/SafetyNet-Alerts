dd4d59e68a74645f507872109a389feeea34349c
Guillaume Aubert <8095025-Kormul@users.noreply.gitlab.com>
2021-09-27 20:07:33 +0200

    Add Custom Url to main

--- src/main/java/com/safetynet/alerts/SafetyNetAlertsApplication.java
+++ src/main/java/com/safetynet/alerts/SafetyNetAlertsApplication.java
@@ -1,13 +1,24 @@
 package com.safetynet.alerts;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@lombok.Data
 @SpringBootApplication
-public class SafetyNetAlertsApplication {
+public class SafetyNetAlertsApplication implements CommandLineRunner{
+	
+	@Autowired
+	private DefineApiUrl customUrl;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SafetyNetAlertsApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
 	}
 
 }
