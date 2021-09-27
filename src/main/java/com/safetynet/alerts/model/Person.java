3f27c115b438b0f2e7b4bc1e36ec49b2838f255d
Guillaume Aubert <8095025-Kormul@users.noreply.gitlab.com>
2021-09-27 20:09:22 +0200

    Create Model Personn

---
+++ src/main/java/com/safetynet/alerts/model/Person.java
@@ -0,0 +1,34 @@
+package com.safetynet.alerts.model;
+
+import javax.validation.constraints.NotNull;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Person {
+	
+
+	@NotNull(message = "Person : Name is null")
+	private String firstName;
+	
+	@NotNull(message = "Person : lastName is null")
+	private String lastName;
+	
+	@NotNull(message = "Person : address is null")
+	private String address;
+	
+	@NotNull(message = "Person : city is null")
+	private String city;
+	
+	@NotNull(message = "Person : zip is null")
+	private String zip;
+	
+	@NotNull(message = "Person : phone is null")
+	private String phone;
+	
+	@NotNull(message = "Person : email is null")
+	private String email;
+
+}
