437ecfc7877683e8c6d80ecf24af67ab33da2429
Guillaume Aubert <8095025-Kormul@users.noreply.gitlab.com>
2021-09-27 20:14:03 +0200

    Create model Data

---
+++ src/main/java/com/safetynet/alerts/model/Data.java
@@ -0,0 +1,51 @@
+package com.safetynet.alerts.model;
+
+import java.util.List;
+
+@lombok.Data
+public class Data {
+	
+	private List<Person> persons;
+	private List<FireStation>firestations;
+	private List<MedicalRecord> medicalrecords;
+
+	public Person getPersonWithName(String firstName, String lastName) {
+		
+		for(Person person : persons) {
+			if(person.getFirstName().equalsIgnoreCase(firstName) && person.getLastName().equalsIgnoreCase(lastName))
+				return person;
+		}
+		return null;
+		
+	}
+	
+	public String getFirestationsWithAddress(String Address) {
+		for(FireStation firestation : firestations) {
+			if(Address.equalsIgnoreCase(firestation.getAddress()))
+				return firestation.getStation();
+		}
+		return "";
+	}
+	
+	public String getBirthdayWithname(String firstName, String lastName) {
+		
+		for(MedicalRecord medicalRecord : medicalrecords) {
+			if(firstName.equalsIgnoreCase(medicalRecord.getFirstName())) {
+				if(lastName.equalsIgnoreCase(medicalRecord.getLastName())) {
+					return medicalRecord.getBirthdate();
+				}
+			}
+		}
+		return "";
+	}
+	
+	public MedicalRecord getMedicalRecordWithName(String firstName, String lastName) {
+		for(MedicalRecord medicalRecord : medicalrecords) {
+			if(medicalRecord.getFirstName().equalsIgnoreCase(firstName) && medicalRecord.getLastName().equalsIgnoreCase(lastName)) {
+				return medicalRecord;
+			}
+		}
+		return null;
+	}
+
+}
